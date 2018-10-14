c00e0e4083cae6095f004932205443b640124b6c
Andrew Reis <areis89@gmail.com>
2018-10-13 21:49:10 -0400

    type on DATABASE_URL

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const db = new Sequelize(
-  process.env.DATBASE_URL || 'postgres://localhost:5432/acme_store',
+  process.env.DATABASE_URL || 'postgres://localhost:5432/acme_store',
   { logging: false }
 );
 
